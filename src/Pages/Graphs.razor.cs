3bd2cf4c112c946f60b64082967e12237209236d
Len Mahaffey <len.mahaffey@gmail.com>
2023-07-23 09:36:33 -0600

    add d3 and an example bar chart

---
+++ src/Pages/Graphs.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Blazor.Frontend.Pages
+{
+    public partial class Graphs
+    {
+        [Inject] public IJSRuntime? Js { get; set; }
+        private IJSObjectReference? _js;
+        private ElementReference svg;
+
+        protected async override Task OnInitializedAsync()
+        {
+            _js = await Js!.InvokeAsync<IJSObjectReference>("import", "./Pages/Graphs.razor.js");
+            await _js.InvokeVoidAsync("drawBarChart", svg);
+        }
+    }
+}
