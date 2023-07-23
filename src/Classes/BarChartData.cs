e86896ff8072f92d7b11bd46fa45230ba72ffcab
Len Mahaffey <len.mahaffey@gmail.com>
2023-07-23 10:53:53 -0600

    bar chart displaying passed data

---
+++ src/Classes/BarChartData.cs
@@ -0,0 +1,11 @@
+﻿namespace Blazor.Frontend.Classes
+{
+    public class BarChartData
+    {
+        public string Name { get; set; } = string.Empty;
+        public double Value { get; set; }
+        public string ValueAsString { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
--- src/Pages/Graphs.razor.cs
+++ src/Pages/Graphs.razor.cs
@@ -5,14 +5,5 @@ namespace Blazor.Frontend.Pages
 {
     public partial class Graphs
     {
-        [Inject] public IJSRuntime? Js { get; set; }
-        private IJSObjectReference? _js;
-        private ElementReference svg;
-
-        protected async override Task OnInitializedAsync()
-        {
-            _js = await Js!.InvokeAsync<IJSObjectReference>("import", "./Pages/Graphs.razor.js");
-            await _js.InvokeVoidAsync("drawBarChart", svg);
-        }
     }
 }
