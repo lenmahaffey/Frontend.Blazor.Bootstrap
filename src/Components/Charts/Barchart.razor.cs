30465991a3527d5f8eb2ccac792d74cb81153e7b
Len Mahaffey <len.mahaffey@gmail.com>
2023-07-23 23:28:25 -0600

    chart rendering

--- src/Components/Charts/Barchart.razor.cs
+++ src/Components/Charts/Barchart.razor.cs
@@ -6,7 +6,11 @@ namespace Blazor.Frontend.Components.Charts
 {
     public partial class BarChart
     {
-        [Parameter] public List<BarChartData> data { get; set; } = new List<BarChartData>();
+        [Parameter] public List<BarChartData> displayData { get; set; } = new List<BarChartData>();
+        [Parameter] public string xAxisName { get; set; } = "x-axis";
+        [Parameter] public string yAxisName { get; set; } = "y-axis";
+        [Parameter] public string Title { get; set; } = "Bar Chart";
+
         private BlazorBootstrap.BarChart? barChart;
         private BlazorBootstrap.LineChart? lineChart;
 
@@ -21,17 +25,18 @@ namespace Blazor.Frontend.Components.Charts
         {
             var data = new ChartData
             {
-                Labels = this.data.Select(x => x.Name).ToList(),
-                //Labels = new List<string> { "One", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20" },
+                Labels = this.displayData.Select(x => x.Name).ToList(),
                 Datasets = new List<IChartDataset>()
                 {
                     new BarChartDataset()
                     {
-                        Label = "India",
-                        Data = this.data.Select(x => x.Value).ToList(),
+                        Data = displayData.Select(x => x.Value).ToList(),
                         BackgroundColor = new List<string>{ "rgb(88, 80, 141)" },
                         CategoryPercentage = 0.8,
                         BarPercentage = 1,
+                        //HoverBackgroundColor = new List<string>{ "rgb(8, 8, 14)" },
+                        //BorderColor = new List<string>{ "rgb(8, 8, 14)" },
+                        //BorderWidth = new List<double>{ 5.0 }
                     }
                 }
             };
@@ -42,16 +47,15 @@ namespace Blazor.Frontend.Components.Charts
 
             options.Plugins.Title.Text = "MANHATTAN";
             options.Plugins.Title.Display = true;
-            options.Plugins.Title.Font.Size = 20;
+            options.Plugins.Title.Font.Size = 40;
 
             options.Responsive = true;
 
-            options.Scales.X.Title.Text = "Overs";
+            options.Scales.X.Title.Text = xAxisName;
             options.Scales.X.Title.Display = true;
 
-            options.Scales.Y.Title.Text = "Runs";
+            options.Scales.Y.Title.Text = yAxisName;
             options.Scales.Y.Title.Display = true;
-
             await barChart.UpdateAsync(data, options);
         }
 
--- src/Pages/Graphs.razor.cs
+++ src/Pages/Graphs.razor.cs
@@ -1,6 +1,22 @@
-﻿namespace Blazor.Frontend.Pages
+﻿using Blazor.Frontend.Classes;
+
+namespace Blazor.Frontend.Pages
 {
     public partial class Graphs
     {
+        public List<BarChartData> data {get; set; } =
+            new List<BarChartData>
+            {
+                { new BarChartData{ Name = "Rust", Value = 78.9  }},
+                { new BarChartData { Name = "Kotlin", Value = 75.1 } },
+                { new BarChartData { Name = "Python", Value = 68.0 } },
+                { new BarChartData { Name = "TypeScript", Value = 67.0 } },
+                { new BarChartData { Name = "Go", Value = 65.6 } },
+                { new BarChartData { Name = "Swift", Value = 65.1 } },
+                { new BarChartData { Name = "Javascript", Value = 61.9 } },
+                { new BarChartData { Name = "C#", Value = 60.4 } },
+                { new BarChartData { Name = "F#", Value = 59.6 } },
+                { new BarChartData { Name = "Clojure", Value = 59.6 } },
+            };
     }
 }
