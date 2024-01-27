e4ac69cb2fee6e24293b7b2eced515c7827fa468
Len Mahaffey <len.mahaffey@gmail.com>
2024-01-27 16:48:08 -0700

    add pipleline config files

---
+++ src/jenkins.pipeline.groovy
@@ -0,0 +1,85 @@
+pipeline {
+    agent any
+    stages {
+        stage('Clean Workspace') {
+            steps {
+                script {
+                    now = new Date()
+                    currentBuild.displayName = 'frontend.blazor.bootstrap - ' + now.format("yyyy-MM-dd HH:mm")
+                }
+                cleanWs()
+            }
+        }
+        stage('Clone Repository') {
+            steps {
+                git credentialsId: 'personal', url: 'git@github.com:lenmahaffey/frontend.blazor.bootstrap.git'
+            }
+        }
+        
+        stage('Clean Project') {
+            steps {
+                dotnetClean sdk: '8.0'
+            }
+        }
+        
+        stage ('Dotnet Tool Restore') {
+          steps {
+                dotnetToolRestore sdk: '8.0', toolManifest: 'src/.config/dotnet-tools.json'
+            }
+        }
+        
+        stage ('NuGet Package Restore') {
+          steps {
+                dotnetRestore sdk: '8.0'
+            }
+        }
+        
+        stage ('Build') {
+          steps {
+                dotnetBuild  sdk: '8.0'
+            }
+        }
+        
+        stage ('Test') {
+          steps {
+                dotnetTest  noBuild: true, logger: 'trx', resultsDirectory: './TestResults', sdk: '8.0'
+            }
+        }
+        
+        stage ('Publish') {
+          steps {
+                dotnetPublish noBuild: true, outputDirectory: './build', sdk: '8.0', selfContained: false
+            }
+        }
+        
+        stage ('Archive') {
+            steps{
+                archiveArtifacts artifacts: 'build/*', followSymlinks: false
+            }
+        }
+        
+        stage ('Testing Report') {
+            steps{
+                mstest testResultsFile:"**/*.trx", keepLongStdio: true
+            }
+        }
+        
+        stage ('Build Docker Container'){
+            steps{
+                script{
+                    image = docker.build('frontend.blazor.bootstrap', '-f  ./src/Frontend.Blazor.Bootstrap.Dockerfile .')
+                }
+            }
+        }
+        stage('Push image') {
+            steps{
+                script{
+                    docker.withRegistry("https://docker.lenmahaffey.com", "dockerLogin") {
+                        image.push('latest')
+                        image.push(now.format("yyyy-MM-dd"))
+                    }
+                }
+            }
+        } 
+    }
+}
